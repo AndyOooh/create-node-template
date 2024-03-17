4a19f9c085c6c6732bba72be41138850101336c7
AndyOoh <andy.oeee@gmail.com>
2024-03-17 15:47:43 +0700

    add folder for frameworks/vanilla node

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,42 @@
 #! /usr/bin/env node
 import inquirer from 'inquirer';
-import { inpuQuestions } from './examples/input.js';
-import { listInquiry } from './examples/list.js';
-import { packageManagers } from './examples/checkbox.js';
+import { inpuQuestions } from './inquirer/examples/input.js';
+import { listInquiry } from './inquirer/examples/list.js';
+import { packageManagers } from './inquirer/examples/checkbox.js';
+import { runner } from './config/index.js';
+import { runWithCommander } from './commander/intex.js';
+import { runWithInquirer } from './inquirer/index.js';
+import { runWithNode } from './vanilla-node/inde.js';
 
 console.log('🤡🧪 Helloooo World!');
 
 /**
  * Input prompt example
  */
-const main = async () => {
+const run = async () => {
   // Select package manager
   // Select name of project
   // Start node project with -y command using the chosen package manager
 
   // const questions = inpuQuestions
-  const questions = listInquiry;
-  const answers: Record<string, any> = await inquirer.prompt(questions);
-  console.log(JSON.stringify(answers, null, '  '));
+  
   // inquirer.prompt(questions).then((answers: Record<string, any>) => {
   //   console.log(JSON.stringify(answers, null, '  '));
   // });
 
   // const packManagers = packageManagers;
   // console.log('🚀  packManagers:', packManagers);
+
+  switch (runner) {
+    case 'inquirer':
+      runWithInquirer();
+      break;
+    case 'commander':
+      runWithCommander();
+      break;
+    default:
+      runWithNode();
+  }
 };
 
-main();
+run();
---
+++ src/inquirer/index.ts
@@ -0,0 +1,8 @@
+import inquirer from 'inquirer';
+import { listInquiry } from './examples/list.js';
+
+export const runWithInquirer = async () => {
+  const questions = listInquiry;
+  const answers: Record<string, any> = await inquirer.prompt(questions);
+  console.log(JSON.stringify(answers, null, '  '));
+};
