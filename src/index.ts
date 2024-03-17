18b54d23c2499e4a4517efaf1118b2ef5f43e21d
AndyOoh <andy.oeee@gmail.com>
2024-03-17 15:51:16 +0700

    add readme

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
 #! /usr/bin/env node
-import inquirer from 'inquirer';
-import { inpuQuestions } from './inquirer/examples/input.js';
-import { listInquiry } from './inquirer/examples/list.js';
-import { packageManagers } from './inquirer/examples/checkbox.js';
 import { runner } from './config/index.js';
 import { runWithCommander } from './commander/intex.js';
 import { runWithInquirer } from './inquirer/index.js';
@@ -11,22 +7,9 @@ import { runWithNode } from './vanilla-node/inde.js';
 console.log('🤡🧪 Helloooo World!');
 
 /**
- * Input prompt example
+ * ??
  */
 const run = async () => {
-  // Select package manager
-  // Select name of project
-  // Start node project with -y command using the chosen package manager
-
-  // const questions = inpuQuestions
-  
-  // inquirer.prompt(questions).then((answers: Record<string, any>) => {
-  //   console.log(JSON.stringify(answers, null, '  '));
-  // });
-
-  // const packManagers = packageManagers;
-  // console.log('🚀  packManagers:', packManagers);
-
   switch (runner) {
     case 'inquirer':
       runWithInquirer();
--- src/inquirer/index.ts
+++ src/inquirer/index.ts
@@ -2,6 +2,18 @@ import inquirer from 'inquirer';
 import { listInquiry } from './examples/list';
 
 export const runWithInquirer = async () => {
+  // Select package manager
+  // Select name of project
+  // Start node project with -y command using the chosen package manager
+
+  // const questions = inpuQuestions
+
+  // inquirer.prompt(questions).then((answers: Record<string, any>) => {
+  //   console.log(JSON.stringify(answers, null, '  '));
+  // });
+
+  // const packManagers = packageManagers;
+  // console.log('🚀  packManagers:', packManagers);
   const questions = listInquiry;
   const answers: Record<string, any> = await inquirer.prompt(questions);
   console.log(JSON.stringify(answers, null, '  '));
