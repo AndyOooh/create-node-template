a9cff6c0d74780367e6bdcb01292b249fb3e5dff
AndyOoh <andy.oeee@gmail.com>
2024-03-17 23:46:16 +0700

    trim gitignore

--- src/vanilla-node/index.ts
+++ src/vanilla-node/index.ts
@@ -6,7 +6,7 @@ import { execSync } from 'child_process';
 export const runWithNode = async () => {
     // check if project-name is provided
     // If not promt for project-name
-    // Prompt to choose package manager. Npm should be default. Otehr choices: yarn, pnpm, bun
+    // Prompt to choose package manager. Npm should be default. Other choices: yarn, pnpm, bun
     // Prompt to choose project type. Default is node basic. Other choices: node advanced, express, express advanced.
     // Based on project type we will copy the appropriate template from templates folder to the project folder and create the appropriate package.json file. 
 
