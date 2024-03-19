e07315d39af6f9b60f71943599dc186b6e5a2695
AndyOoh <andy.oeee@gmail.com>
2024-03-19 22:37:29 +0700

    fix remplate name

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,12 @@
 export const runner: 'node' | 'inquirer' | 'commander' = 'node';
 
 export type PackageManager = 'npm' | 'yarn' | 'pnpm' | 'bun';
-export type Template = 'node-basic' | 'node-advanced' | 'express' | 'express-advanced';
+export type Template = 'node-basic' | 'node-advanced' | 'express-basic' | 'express-advanced';
 
 export const supportedPMs: PackageManager[] = ['npm', 'yarn', 'pnpm', 'bun'];
 export const supportedTemplates: Template[] = [
   'node-basic',
   'node-advanced',
-  'express',
+  'express-basic',
   'express-advanced',
 ];
