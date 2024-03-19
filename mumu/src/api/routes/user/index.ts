feb7cfef296629474b3b7f2fa6da7cf5109e2e79
AndyOoh <andy.oeee@gmail.com>
2024-03-20 00:27:18 +0700

    delete gen folders

--- mumu/src/api/routes/user/index.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getUsers } from '@controllers/users';
-
-const router = express.Router();
-
-router.route('/').get(getUsers);
-// .post(usersValidator.register, createUser);
-
-export { router as usersRoutes };
