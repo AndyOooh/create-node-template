d0b81500cf3e16a0941e79cb805fce152e24409e
AndyOoh <andy.oeee@gmail.com>
2024-03-21 00:34:34 +0700

    add eslint to express-adv

---
+++ templates/express-advanced/eslint.config.js
@@ -0,0 +1,34 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+/* NB: "eslint.experimental.useFlatConfig": true, is required in VSCode for tseslint.config*/
+
+export default tseslint.config(
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  // ...tseslint.configs.recommendedTypeChecked,
+  // ...tseslint.configs.strictTypeChecked,
+  // ...tseslint.configs.stylisticTypeChecked,
+
+  /* This block is required for type checked */
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    ignores: ['node_modules', 'dist', 'templates'],
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'no-useless-catch': 'warn',
+    },
+  }
+);
