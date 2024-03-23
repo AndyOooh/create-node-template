493f825a9f92ffa2cec9564bdf85dea2d0fe6b9d
AndyOoh <andy.oeee@gmail.com>
2024-03-23 17:13:45 +0700

    modify asyncHandler

--- templates/express-advanced/src/controllers/users/index.ts
+++ templates/express-advanced/src/controllers/users/index.ts
@@ -2,10 +2,7 @@ import { Request, Response } from 'express';
 import { fetchUsers } from '@services/user';
 
 export const getUsers = async (_req: Request, res: Response) => {
-  try {
-    const users: unknown = await fetchUsers();
-    res.status(200).send(users);
-  } catch (error) {
-    res.status(500).send;
-  }
+  const users: unknown = await fetchUsers();
+  if (!users) throw new Error('No users found.');
+  res.status(200).send(users);
 };
