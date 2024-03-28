cba95d9cdfc87d759c42203bbd109f70e6fa7f66
AndyOoh <andy.oeee@gmail.com>
2024-03-29 00:32:16 +0700

    add lint scripits

--- yammuio/src/controllers/users/index.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-import { fetchUsers } from '@services/user/index.js';
-import { AsyncHandler } from 'src/api/middlewares/asyncHandler.js';
-
-export const getUsers: AsyncHandler = async (_req: Request, res: Response): Promise<void> => {
-  const users: unknown = await fetchUsers();
-  if (!users) throw new Error('No users found.');
-  res.status(200).send(users);
-};
--- yammuio/src/loaders/index.ts
+++
@@ -1,9 +0,0 @@
-import { Application } from 'express';
-import { expressLoader } from './express.js';
-
-export const initLoaders = (app: Application) => {
-  expressLoader(app);
-  console.log('Express Intialized');
-
-  // ... Add more loaders, e.g. agenda, Redis, Mongoose, etc.
-};
